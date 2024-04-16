6cbe94faa5b3e022ffa8bacf1dbdaf7d629d79f1
WillV12 <120140873+WillV12@users.noreply.github.com>
2024-04-16 13:50:03 -0400

    Intro Made-Will 4/16/2024
    Added the intro text and function to print it out

---
+++ Story.py
@@ -0,0 +1,16 @@
+
+import random
+from time import sleep
+# Will Vanderploeg:
+# Displays the intro one line at a time using the intro.txt file
+def intro():
+    year = random.randrange(224, 8427)
+    print(f"\n\nThe year is {year}")
+    sleep(1)
+    intro = open("intro.txt", "r")
+    for i in range(12):
+        print(intro.readline())
+        sleep(1)
+    intro.close()
+
+intro()
