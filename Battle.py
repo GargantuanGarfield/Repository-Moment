538fdd515192a9b862111493ce406f395387cfde
Nwwhitton <nwwhitton@my.waketech.edu>
2024-04-17 12:59:10 -0400

    Made functionality with myself

--- Battle.py
+++ Battle.py
@@ -1,44 +1,114 @@
 import time  # Nicholas: for pauses
 import random  # Nicholas: implement chance in moves
 import Questions
+import Player
 Option_battle_list = ['Attack', 'Block']
 Quiz_Types = ["MC", "JEOPARDY", "FITB", "TOF"]  # Nicholas: List of the quiz types for the if/else statements
+ehp = 50
+php = 50
+edmg = 1
+pdmg = 1
 def battling(enemy, player):
-    while enemy.hp > 0 and player.hp:  # Nicholas: code runs until player or enemy dies
+    global ehp, php
+    battle_choice = input(f'Choose what to do {Option_battle_list}: ').title()
+    while ehp > 0 and php > 0:  # Nicholas: code runs until player or enemy dies
 
-        battle_choice = input(f'Choose what to do {Option_battle_list}: ').title()
         while battle_choice not in Option_battle_list:  # runs until battle_choice is valid
             battle_choice = input(f'Choose what to do {Option_battle_list}: ').title()
 
         if battle_choice == 'Attack':
 
-            quiz_choice = input(f'you can choose: {Quiz_Types}: ').capitalize()
-            if quiz_choice in Quiz_Types:
-                if quiz_choice == 'MC':
-                    pass
-                        # TODO enemy.hp -= player.atk
-                        # TODO print(f'{enemy.name} has {enemy.hp} health!')
+            good2go = False
+            while not good2go:
+                for room in Quiz_Types:
+                    print('[' + room[0] + ']' + room[1:] + ": ", end="")
+                move = input("\n\tInput What you would like to do (M/J/F/T): ").upper()
+                for i in range(len(Quiz_Types)):
+                    if len(move) > 1:
+                        if move == Quiz_Types[i][0:len(move)].upper():
+                            move = Quiz_Types[i]
+                            if move == "TOF":
+                                print("easy")
+                                ehp -= pdmg
 
-                elif quiz_choice == 'JEOPARDY':
-                    pass
-                        # TODO ratHealth -= player.atk
-                        # TODO print(f'{enemy.name} has {enemy.hp} health!')
 
-                elif quiz_choice == 'FITB':
-                    pass
-                        # TODO enemy.hp -= player.atk
-                        # TODO print(f'{enemy.name} has {enemy.hp} health!')
 
-                elif quiz_choice == 'TOF':
-                    pass
-                        # TODO enemy.hp -= player.atk
-                        # TODO print(f'{enemy.name} has {enemy.hp} health!')
-                else:
-                    print('Not a move try again.')
-                # Nicholas: TODO Must add player lose health
+                            elif move == "MC":
+                                print("med")
+                                ehp -= pdmg + 1
+
+
+                            elif move == "FITB":
+                                print("hard")
+                                # Player.atk("hard")
+                                ehp -= pdmg + 2
+
+                            elif move == "JEOPARDY":
+                                print("ntmare")
+                                # Player.atk("ntmare")
+                                ehp -= pdmg + 3
+
+
+                            else:
+                                print("How Did it not work")
+
+                            good2go = True
+                            break
+
+                    elif len(move) == 1:
+                        if move == Quiz_Types[i][0]:
+                            move = Quiz_Types[i]
+                            if move == "TOF":
+                                print("easy")
+                                # Player.atk("easy")
+                                ehp -= pdmg
+
+
+                            elif move == "MC":
+                                print("med")
+                                # Player.atk("med")
+                                ehp -= pdmg + 1
+
+
+                            elif move == "FITB":
+                                print("hard")
+                                # Player.atk("hard")
+                                ehp -= pdmg + 2
+
+
+                            elif move == "JEOPARDY":
+                                print("ntmare")
+                                # Player.atk("ntmare")
+                                ehp -= pdmg + 3
+
+
+
+                            else:
+                                print("How Did it not work")
+                            good2go = True
+            while not good2go:
+                if ehp <= 0:
+                    ehp = 0
+                    print(ehp)
+                    print(php)
+                elif php <= 0:
+                    php = 0
+                    print(ehp)
+                    print(php)
+            else:
+                php -= edmg
+                print(ehp)
+                print(php)
+                battle_choice = input(f'Choose what to do {Option_battle_list}: ').title()
+
+        elif battle_choice == 'Block':
+            print("UH OH YOU BLOCKED")
+            battle_choice = input(f'Choose what to do {Option_battle_list}: ').title()
+
         elif battle_choice == 'Block':
             pass  # Need To Implement Block
 
+battling("","")
 
 
 
