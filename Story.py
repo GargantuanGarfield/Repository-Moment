ab9e256ee844b3ba9059dc1fee1f4ec1d0fadd37
lgift <lpgift@my.waketech.edu>
2024-04-18 13:16:45 -0400

    andrew glasneirn
    BASFUIP BSDHJFLNKML

--- Story.py
+++ Story.py
@@ -1,6 +1,7 @@
 from time import sleep
 import hazbin
 
+
 # Opens story dialogue leading into the first player battle.
 def battle1():
     battle1 = open('battle1-portland.txt', 'r')
@@ -10,6 +11,7 @@ def battle1():
     battle1.close()
 
 
+# Opens story dialogue leading into the second player battle.
 def battle2():
     battle2 = open('battle2-thieves', 'r')
     for i in range(8):
@@ -18,6 +20,7 @@ def battle2():
     battle2.close()
 
 
+# Opens story dialogue leading into the third player battle.
 def battle3():
     battle3_pt1 = open('battle3-hazbinhotel', 'r')
     battle3_pt2 = open('battle3-hazbinhotel-p2', 'r')
@@ -38,10 +41,19 @@ def battle3():
     battle3_pt2.close()
 
 
+# Opens story dialogue leading into the fourth player battle.
 def battle4():
     battle4 = open('battle4-boss', 'r')
     for i in range(13):
         print(battle4.readline())
         sleep(.87)
+    battle4.close()
 
-battle3()
+
+# Outro dialogue
+def outro():
+    outro = open('outro', 'r')
+    for i in range(14):
+        print(outro.readline())
+        sleep(.87)
+    outro.close()
